d3d5776dfaa4760fd35bcd7a393d252d9c405dc0
rauldesarrolla <xceptionalbro@gmail.com>
2019-05-21 10:39:22 +0200

    Añadido reloj

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ import pygame as pg
 from pygame.locals import *
 
 size = H, W = 680, 400
+framerate = 1
 
 def main():
 	screen = pg.display.set_mode(size)
+	clock = pg.time.Clock()
 
 	background = pg.Surface(size)
 	background = background.convert()
 	background.fill((180, 30, 70))
 
-	screen.blit(background, (0,0))
-
 	while True:
+		clock.tick()
+		# Escuchar eventos
 		for event in pg.event.get():
 			if event.type == QUIT:
 				sys.exit(0)
+		# Actulizar estados de juego
+
+		# Pintar
+		screen.blit(background, (0,0))
 		pg.display.flip()
-		pass
 
 if __name__ == "__main__":
 	pg.init()
