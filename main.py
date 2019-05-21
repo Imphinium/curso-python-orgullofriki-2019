47e2ce4b89e5cc29f7be1117fb4b9cb7d67be4b6
rauldesarrolla <xceptionalbro@gmail.com>
2019-05-21 10:44:00 +0200

    Añadido cubo de pruebas

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame as pg
 from pygame.locals import *
 
 size = H, W = 680, 400
-framerate = 1
+framerate = 60
 
 def main():
 	screen = pg.display.set_mode(size)
@@ -13,8 +13,13 @@ def main():
 	background = background.convert()
 	background.fill((180, 30, 70))
 
+	test = pg.Surface((200, 200))
+	test = test.convert()
+	test.fill((30, 70, 180))
+	i=0
+
 	while True:
-		clock.tick()
+		clock.tick(framerate)
 		# Escuchar eventos
 		for event in pg.event.get():
 			if event.type == QUIT:
@@ -23,6 +28,8 @@ def main():
 
 		# Pintar
 		screen.blit(background, (0,0))
+		screen.blit(test, (i, 200))
+		i+=1
 		pg.display.flip()
 
 if __name__ == "__main__":
