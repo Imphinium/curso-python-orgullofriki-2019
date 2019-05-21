d3b1e8697b608825272174fd1211c420e7796dfd
rauldesarrolla <xceptionalbro@gmail.com>
2019-05-21 10:31:54 +0200

    Añadido background

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ size = H, W = 680, 400
 def main():
 	screen = pg.display.set_mode(size)
 
+	background = pg.Surface(size)
+	background = background.convert()
+	background.fill((180, 30, 70))
+
+	screen.blit(background, (0,0))
+
 	while True:
 		for event in pg.event.get():
 			if event.type == QUIT:
-				sys.exit()
+				sys.exit(0)
 		pg.display.flip()
 		pass
 
